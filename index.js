e67be7c8537edc0ba056bebcea313d0a4a8c6e12
RollyLif <relifungula@gmail>
2022-06-20 22:32:03 +0200

    features: getting name and email in javascript

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let nom=document.querySelector('input');
-nom.addEventListener('change', function(){
-    alert(nom.value());
-});
+let bouton=document.querySelector('#Commencer');
+bouton.onclick = function(){
+    let nom = document.getElementById('nom').value;
+    let email = document.getElementById('email').value;
+    alert(nom+" et "+email);
+};
