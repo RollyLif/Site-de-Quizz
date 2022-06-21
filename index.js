36ef1066744fe69f497b9b95859a6c8af0b618a3
RollyLif <relifungula@gmail>
2022-06-21 10:05:02 +0200

    features : verification of name and email

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ let bouton=document.querySelector('#Commencer');
 bouton.onclick = function(){
     let nom = document.getElementById('nom').value;
     let email = document.getElementById('email').value;
-    alert(nom+" et "+email);
+    let exp= new RegExp("^\\w{2,}$");
+    let exp1=new RegExp("^[a-zA-Z]([a-zA-Z0-9._-]{2,})@([a-z0-9._-]{3,})\.([a-z]{2,})$");
+    if(exp.test(nom)){
+        document.querySelector('.nom_erreur').style.display = 'none';
+    }else{
+        document.querySelector('.nom_erreur').style.display = 'block';
+    }
+    if(exp1.test(email)){
+        document.querySelector('.email_erreur').style.display = 'none';
+    }else{
+        document.querySelector('.email_erreur').style.display = 'block';
+    }
+
 };
