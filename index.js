6dcae835771de2f10121b4d55327a879d7047a8f
RollyLif <relifungula@gmail>
2022-07-01 14:55:11 +0200

    refact:prendre en compte le nom ayant de caractère accentué

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if(bouton){
     bouton.onclick = function(){
         let nom = document.getElementById('nom').value;
         let email = document.getElementById('email').value;
-        let exp= new RegExp("^\\w{2,}$");
+        let exp= new RegExp("^[a-zA-Zéèêëçàîïô_-]{2,}$");
         let exp1=new RegExp("^[a-zA-Z]([a-zA-Z0-9._-]{2,})@([a-z0-9._-]{3,})\.([a-z]{2,})$");
         if(exp.test(nom)){
             document.querySelector('.nom_erreur').style.display = 'none';
